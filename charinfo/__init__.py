7d203c903061ec5bc128c146b4f563c21d15a6c5
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2021-10-21 22:02:16 +0600

    Add information to __init__.py

---
+++ charinfo/__init__.py
@@ -0,0 +1,8 @@
+"""A command line tool to view detailed information about a character."""
+
+__name__ = "charinfo"
+__title__ = __name__
+__license__ = "MIT"
+__version__ = "0.1.0"
+__author__ = "Arian Mollik Wasi"
+__github__ = "https://github.com/wasi-master/charinfo"
